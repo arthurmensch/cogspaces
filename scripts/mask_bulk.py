641123edc704f99239bdfa3c3af7c40c26c56f6f
MENSCH Arthur <arthur.mensch@inria.fr>
2018-02-16 21:16:59 +0100

    WIP

--- scripts/mask_bulk.py
+++ scripts/mask_bulk.py
@@ -15,7 +15,7 @@ idx = pd.IndexSlice
 seed = 0
 
 
-def unmask(masker, imgs):
+def mask(masker, imgs):
     return masker.transform(imgs)
 
 
@@ -34,7 +34,8 @@ n_features = masker.mask_img_.get_data().sum()
 new_contrasts = []
 for dataset, these_contrasts in contrasts.groupby('dataset'):
     subjects = these_contrasts.index.get_level_values('subject').unique().values
-    train_subjects, test_subjects = train_test_split(subjects, test_size=.5, random_state=seed)
+    train_subjects, test_subjects = train_test_split(subjects, test_size=.5,
+                                                     random_state=seed)
     train_contrasts = these_contrasts.loc[idx[:, train_subjects], :]
     test_contrasts = these_contrasts.loc[idx[:, test_subjects], :]
     these_contrasts = pd.concat([train_contrasts, test_contrasts], keys=
@@ -52,8 +53,8 @@ for (dataset, fold), these_contrasts in contrasts.groupby(['dataset', 'fold']):
     batches = list(gen_batches(n_samples, batch_size))
     data = Parallel(n_jobs=n_jobs, verbose=10,
                     backend='multiprocessing', mmap_mode='r')(
-        delayed(unmask)(masker, imgs[batch]) for batch in batches)
+        delayed(mask)(masker, imgs[batch]) for batch in batches)
     data = np.concatenate(data, axis=0)
 
-    dump((data, targets, subjects), join(prepare_dir,
-                                         'data_%s_%s.pt' % (dataset, fold)))
+    dump((data, targets, subjects),
+         join(prepare_dir, 'data_%s_%s.pt' % (dataset, fold)))
